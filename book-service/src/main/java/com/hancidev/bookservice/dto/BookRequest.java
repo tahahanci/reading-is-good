d05a30f9261154bddd625c2265c1b11b260b32dc
Taha Hancı <taha.hanci@std.bogazici.edu.tr>
2025-02-13 12:59:10 +0300

    feat: add builder pattern to BookRequest DTO

--- book-service/src/main/java/com/hancidev/bookservice/dto/BookRequest.java
+++ book-service/src/main/java/com/hancidev/bookservice/dto/BookRequest.java
@@ -1,4 +1,7 @@
 package com.hancidev.bookservice.dto;
 
+import lombok.Builder;
+
+@Builder
 public record BookRequest(String bookName, String author, int stockAmount, double price, int publishYear) {
 }
