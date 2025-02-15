31e3b2f016d82172c8556dc504da50dc5717f5f5
Taha Hancı <taha.hanci@std.bogazici.edu.tr>
2025-02-15 16:27:31 +0300

    feat: added builder pattern

--- customer-service/src/main/java/com/hancidev/customerservice/dto/AddressRequest.java
+++ customer-service/src/main/java/com/hancidev/customerservice/dto/AddressRequest.java
@@ -1,4 +1,7 @@
 package com.hancidev.customerservice.dto;
 
+import lombok.Builder;
+
+@Builder
 public record AddressRequest(String street, String city, String state, String zipCode) {
 }
--- customer-service/src/main/java/com/hancidev/customerservice/dto/CustomerRequest.java
+++ customer-service/src/main/java/com/hancidev/customerservice/dto/CustomerRequest.java
@@ -1,7 +1,10 @@
 package com.hancidev.customerservice.dto;
 
+import lombok.Builder;
+
 import java.util.List;
 
+@Builder
 public record CustomerRequest(String customerName, String customerSurname,
                               String customerMail, List<AddressRequest> addressRequests) {
 }
